bec6d3ef10f06cefcef8e8011ffb0cee56012941
Mehdi <karo.k6918@gmail.com>
2023-05-03 04:38:19 +0330

    model add

---
+++ blogs/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Author(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    class Meta:
+        db_table = 'authors'
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def str(self):
+        return self.user.username
+
+
+class Post(BaseModel):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    class Meta:
+        db_table = 'posts'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def str(self):
+        return self.title
