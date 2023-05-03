21ec0b48b75bea8023163ba2b7b8a1b74487a213
Mehdi <karo.k6918@gmail.com>
2023-05-03 05:49:37 +0330

    make view admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Author, Post
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'bio', 'avatar', 'is_active']
+    list_filter = ['is_active', 'create_time', 'update_time']
+    search_fields = ['user__username', 'bio']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'is_active']
+    list_filter = ['is_active', 'create_time', 'update_time']
+    search_fields = ['title', 'author__user__username']
